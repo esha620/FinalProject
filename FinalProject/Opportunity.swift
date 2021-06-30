986058bff5bac9378d5b92d78b001636c19cf6d1
Esha <eshaj20@gmail.com>
2021-06-30 14:06:21 -0400

    iteration 6

---
+++ FinalProject/Opportunity.swift
@@ -0,0 +1,14 @@
+//
+//  Opportunity.swift
+//  FinalProject
+//
+//  Created by Esha Jain on 6/30/21.
+//
+
+import UIKit
+
+class Opportunity{
+    var name = ""
+    var type = ""
+    var important = true
+}
